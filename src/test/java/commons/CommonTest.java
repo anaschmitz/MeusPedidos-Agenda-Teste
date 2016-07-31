908a681814aa1423ca648e6ab851a0354ad11dd5
Ana Paula Schmitz <anaschmitz@gmail.com>
2016-07-31 18:35:26 -0300

    Implementado execução pelo maven

--- src/test/java/commons/CommonTest.java
+++ src/test/java/commons/CommonTest.java
@@ -1,18 +1,28 @@
 package commons;
 
+import java.security.InvalidParameterException;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.Optional;
+import org.testng.annotations.Parameters;
 
 public class CommonTest {
 
 	protected WebDriver driver;
 
 	@BeforeSuite
-	public void initDriver() throws Exception {
-		System.out.println("You are testing in firefox");
-		driver = new FirefoxDriver();
+	@Parameters({ "browser", "url" })
+	public void initDriver(@Optional("firefox") String browser, String url) throws Exception {
+		if (browser.equals("firefox")) {
+			driver = new FirefoxDriver();
+		} else  {
+			throw new InvalidParameterException("Browser " + browser + "not supported!");
+		}
+		driver.get(url);
+		driver.manage().window().maximize();
 	}
 
 	@AfterSuite
--- src/test/java/pages/AgendaTest.java
+++ src/test/java/pages/AgendaTest.java
@@ -3,6 +3,7 @@ package pages;
 import org.openqa.selenium.support.PageFactory;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
 import commons.CommonTest;
@@ -11,63 +12,56 @@ import utils.SeleniumUtils;
 public class AgendaTest extends CommonTest {
 
 	@BeforeClass
-	public void init() throws Exception {
-		driver.get("http://qa.meuspedidos.com.br/login/");
-		driver.manage().window().maximize();
-
+	@Parameters({ "user", "password" })
+	public void init(String user, String password) throws Exception {
 		AuthenticationPage authPage = PageFactory.initElements(driver, AuthenticationPage.class);
 		SeleniumUtils.waitForDocumentReady(driver);
-		authPage.doLogin("anaschmitz@gmail.com", "123456");
-		
-		// verificar se logou
-		
+		authPage.doLogin(user, password);
+
 		MenuPage menuPage = PageFactory.initElements(driver, MenuPage.class);
 		SeleniumUtils.waitForDocumentReady(driver);
 		menuPage.clickAgenda();
-		
-		// verificar se abriu a agenda
 	}
 
 	@Test
 	public void NovoAgendamentoTestCreate() {
 		System.out.println("NovoAgendamentoTestCreate");
 	}
-	
+
 	@Test
 	public void NovoAgendamentoTestRead() {
 		System.out.println("NovoAgendamentoTestRead");
 	}
-	
+
 	@Test
 	public void NovoAgendamentoTestDontSubmitWithOldDate() {
 		System.out.println("NovoAgendamentoTestDontSubmitWithOldDate");
 	}
-	
+
 	@Test
 	public void RegistarAtividadeTestCreate() {
 		System.out.println("RegistarAtividadeTestCreate");
 	}
-	
+
 	@Test
 	public void RegistarAtividadeTestRead() {
 		System.out.println("RegistarAtividadeTestRead");
-	}	
-	
+	}
+
 	@Test
 	public void RegistarAtividadeTestDontSubmitWithFutureDate() {
 		System.out.println("RegistarTestDontSubmitWithFutureDate");
 	}
-	
+
 	@Test
 	public void TestUpdate() {
 		System.out.println("TestUpdate");
 	}
-	
+
 	@Test
 	public void TestDelete() {
 		System.out.println("TestDelete");
 	}
-	
 
 	@AfterClass
 	public void end() throws Exception {
